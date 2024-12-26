4681887aab1d261f792f472a7115f0581d5ff379
vishnudhrona <vishnuvaliyaveetil92@gmail.com>
2024-12-26 14:26:24 +0530

    done

--- src/landingpage/Landingpage.jsx
+++ src/landingpage/Landingpage.jsx
@@ -6,10 +6,8 @@ import { Box } from "@mui/material";
 const Landingpage = () => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Content */}
       <Box
         sx={{
           flexGrow: 1,
@@ -17,8 +15,8 @@ const Landingpage = () => {
           justifyContent: "center",
           alignItems: "center",
           p: 3,
-          minHeight: "100vh", // Ensures full-height centering
-          backgroundColor: "#f9f9f9", // Optional: Add a light background
+          minHeight: "100vh", 
+          backgroundColor: "#f9f9f9",
         }}
       >
         <Productpage />
--- src/weatherpage/Weatherpage.jsx
+++ src/weatherpage/Weatherpage.jsx
@@ -8,10 +8,7 @@ const Weatherpage = () => {
         <>
         <Sidebar />
         <Box sx={{ display: "flex" }}>
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Main Content */}
       <Box
         sx={{
           flexGrow: 1,
@@ -19,8 +16,8 @@ const Weatherpage = () => {
           justifyContent: "center",
           alignItems: "center",
           p: 3,
-          minHeight: "100vh", // Ensures full-height centering
-          backgroundColor: "#f9f9f9", // Optional: Add a light background
+          minHeight: "100vh",
+          backgroundColor: "#f9f9f9",
         }}
       >
         <div>
